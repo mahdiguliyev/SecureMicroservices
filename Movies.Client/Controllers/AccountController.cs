cc7e2c96f00c5ab282132758ca28cab1a8246057
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-07-02 19:51:02 +0400

    added Access Denied page for Claim based Authorization

---
+++ Movies.Client/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Client.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
